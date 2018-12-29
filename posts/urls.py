d5eda44434d5d936c838988cc89de3d1e6bc9333
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-12-29 17:14:31 +0000

    Typo Added superuser: admin

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import get_posts, post_details, create_or_edit_post
 
 urlpatterns = [
         url(r'^$', get_posts, name = "get_posts"),
-        url(r'^(?P<pk>\d+)/$', post_details, name = 'post_details').
+        url(r'^(?P<pk>\d+)/$', post_details, name = 'post_details'),
         url(r'^new/$', create_or_edit_post, name = 'new_post'),
         url(r'^(?P<pk>\d+)/edit/$', create_or_edit_post, name = 'edit_post')
     ]
