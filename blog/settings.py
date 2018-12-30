7395c8107030b6e0bf4652dc5f868dbcc9b673b7
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-12-30 13:25:10 +0000

    Another typo...

--- blog/settings.py
+++ blog/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABSE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     print('DATABASE_URL not found, using sqlite instead')
